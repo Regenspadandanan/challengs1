e15ff1c5072b9282b87ab713cf4f67c2785d5d58
REGENS <incessregens14@gmail.com>
2021-10-12 14:21:02 +0800

    first commit

---
+++ BiodataDiri.java
@@ -0,0 +1,22 @@
+public class BiodataDiri{
+
+    public static void main(String[] args){
+    String firstName = "Regena";
+    String lastName = "Sherly";
+    String thirdName = "Padandanan";
+    int age = 22;
+    String address = "Makassar";
+    String hobby = "Memasak";
+    String favoritefood = "kapurung";
+    String phonenumber= "082158975443";
+    System.out.println("first Name :" + firstName);
+    System.out.println("last Name :" + lastName);
+    System.out.println("third Name :" + thirdName);
+    System.out.println("age :" + age);
+    System.out.println("address :" + address);
+    System.out.println("hobby  :" + hobby);
+    System.out.println("phonenumber :" + phonenumber);
+
+
+    }
+}
